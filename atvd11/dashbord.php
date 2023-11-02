1b5e23c7f570495b95fc2741947eddcbd8770060
VitorLuckmann <vitorlk.trabalho@gmail.com>
2023-11-02 20:05:24 -0300

    Update dashbord.php

--- atvd11/dashbord.php
+++ atvd11/dashbord.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if (!isset($_SESSION["jovenir"]) || !isset($_SESSION["login_time"]) || !isset($_SESSION["last_request_time"])) {
+    if (!isset($_SESSION["Vitor"]) || !isset($_SESSION["login_time"]) || !isset($_SESSION["last_request_time"])) {
         echo '<script>alert("Os dados da sessão foram perdidos. Por favor, faça login novamente.");</script>';
         header("Location: login.php");
         exit();
@@ -10,7 +10,7 @@
     $_SESSION["last_request_time"] = time();
     $session_duration = time() - $_SESSION["login_time"];
 
-    echo "Bem-vindo, " . $_SESSION["jovenir"] . "!<br>";
+    echo "Bem-vindo, " . $_SESSION["Vitor"] . "!<br>";
     echo "Data/hora de início da sessão: " . date("Y-m-d H:i:s", $_SESSION["login_time"]) . "<br>";
     echo "Data/hora da última requisição: " . date("Y-m-d H:i:s", $_SESSION["last_request_time"]) . "<br>";
     echo "Tempo de sessão: " . gmdate("H:i:s", $session_duration);
